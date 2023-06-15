01a4be89aecbcd91df8625996254c66283be0737
PeteNelson22 <peternelson063@gmail.com>
2023-06-15 11:36:07 +0100

    Handle errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,10 +1,26 @@
+import Link from 'next/link'
+
 const WebSearch = async ({ searchParams }) => {
   const res =
     await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}
 `)
+  if (!res.ok) {
+    throw new Error('Something went wrong')
+  }
   const data = await res.json()
   const results = data.items
 
+  if (!results) {
+    return (
+      <div className='flex flex-col justify-center items-center pt-10'>
+        <h1 className='text-3xl mb-4'> No results found</h1>
+        <p className='text-lg'>
+          Try searching something else or go back to home page{' '}
+          <Link href='/' className='text-blue-500'>Home</Link>
+        </p>
+      </div>
+    )
+  }
   return (
     <>
       {results &&
