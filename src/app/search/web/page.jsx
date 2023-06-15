0f0c734aa1a71c5c065fdb45852505ae9889eb78
PeteNelson22 <peternelson063@gmail.com>
2023-06-15 11:07:29 +0100

    fetch data from google api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,6 +1,15 @@
-const WebSearch = () => {
+const WebSearch = async ({ searchParams }) => {
+  const res =
+    await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}
+`)
+  const data = await res.json()
+  const results = data.items
+
   return (
-    <div>WebSearch</div>
+    <>
+      {results &&
+        results.map((result) => <h1 key={result.cacheId}>{result.title}</h1>)}
+    </>
   )
 }
 export default WebSearch
