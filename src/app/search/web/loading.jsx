86138b7da8680e31e37958642db83ff040589f9f
nelson <peternelson063@gmail.com>
2023-06-21 10:45:23 +0100

    Added loading effect

---
+++ src/app/search/web/loading.jsx
@@ -0,0 +1,25 @@
+const loading = () => {
+  return (
+    <>
+      <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className='h-2.5 w-48 bg-gray-200 rounded-full mb-2'></div>
+        <div className='h-3.5 max-w-[360px] bg-gray-200 rounded-full mb-2'></div>
+        <div className='h-2.5 max-w-[560px] bg-gray-200 rounded-full mb-2'></div>
+        <div className='h-2.5 max-w-[530px] bg-gray-200 rounded-full mb-2'></div>
+      </div>
+      <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className='h-2.5 w-48 bg-gray-200 rounded-full mb-2'></div>
+        <div className='h-3.5 max-w-[360px] bg-gray-200 rounded-full mb-2'></div>
+        <div className='h-2.5 max-w-[560px] bg-gray-200 rounded-full mb-2'></div>
+        <div className='h-2.5 max-w-[530px] bg-gray-200 rounded-full mb-2'></div>
+      </div>
+      <div className="mx-2 pt-10 max-w-6xl lg:pl-52 animate-pulse">
+        <div className='h-2.5 w-48 bg-gray-200 rounded-full mb-2'></div>
+        <div className='h-3.5 max-w-[360px] bg-gray-200 rounded-full mb-2'></div>
+        <div className='h-2.5 max-w-[560px] bg-gray-200 rounded-full mb-2'></div>
+        <div className='h-2.5 max-w-[530px] bg-gray-200 rounded-full mb-2'></div>
+      </div>
+    </>
+  );
+};
+export default loading;
